fcee927bd8b03e3afbda4be2bf18846bd8e0929c
Fernanda <fernanda@Fernandas-iMac.local>
2021-07-09 12:20:46 -0300

    começando a montar o tabuleiro, com bugs

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
+
+
 const tabuleiro = document.querySelector(".tabuleiro")
-tabuleiro.innerHTML += '<div class="branco"></div>';
+
+function div (classHTML, content = "") {
+    return `<div class=${classHTML}>${content}</div>`
+}
+
+
+const preto = div("preto")
+const branco = div("branco")
+
+
+
+
+for (let numeroLinha = 0; numeroLinha < 7; numeroLinha++) {
+    // const element = array[index];
+    if (tabuleiro.childElementCount % 2) {
+        tabuleiro.innerHTML += `<div class="linha">${preto} ${branco}</div>`
+    } else {
+        tabuleiro.innerHTML += `<div class="linha">${branco} ${preto}</div>`
+    }
+    
+}
