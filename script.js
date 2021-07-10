ddb70cc2dc835d02604837ea44249bfdc08e9f5d
Gabriel Rezende <g@brielrezen.de>
2021-07-09 22:41:10 -0300

    simplicado

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const board = document.querySelector(".board")
 
-function div (classHTML, content = "") {
-    return `<div class=${classHTML}>${content}</div>`
+function div (classHTML) {
+    return `<div class=${classHTML}></div>`
 }
 
-const black = div("black")
-const white = div("white")
-const two = black + white
-const seven = two.repeat(3) + black
-
-for (let line = 0; line < 8; line++) {
-    if (board.childElementCount % 2) {
-        board.innerHTML +=  div('line', seven + white)
+for (let quadro = 0; quadro < 72; quadro++) {
+    if (Number.isInteger(quadro / 9)) {
+        quadro++
+    } 
+    if (quadro % 2) {
+        board.innerHTML +=  div("white")
     } else {
-        board.innerHTML += div('line', white + seven)
-    }
+        board.innerHTML += div("black")
+    } 
 }
